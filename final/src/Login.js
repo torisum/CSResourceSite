55d306e41375357e45d6a92484d311137b8c7269
Lizzet Clifton <lc244@duke.edu>
2018-10-18 11:09:52 -0400

    added initial styling and content to login page

--- final/src/Login.js
+++ final/src/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './Login.css';
 import {BrowserRouter as Router, Link} from 'react-router-dom';
 import { withRouter } from "react-router-dom";
 import Route from 'react-router-dom/Route'
@@ -9,7 +9,23 @@ import Route from 'react-router-dom/Route'
 class Login extends Component {
   render() {
     return (
-      <h1>This is the Login Page</h1>
+      <div id="Login-square">
+        <div id= "left-side-login">
+
+            <p id="name">Name</p>
+            <p id="caption">New Connections in Tech</p>
+            <div id="descriptions">
+              <p class="adjectives">Adjective</p>
+              <p class="adjectives">Other Adjective</p>
+              <p class="adjectives">Last Adjective</p>
+            
+          </div>
+        </div>
+        <div id="right-side-login">
+          <p id="hello">Hello</p>
+          <p id="enter">Enter your Login information.</p>
+        </div>
+      </div>
     );
   }
 }
